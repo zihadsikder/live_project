33f8ca270bf42854b7cdd08cc95d0ac5d578828f
zihadsikder <zihadsikder.cse@gmail.com>
2023-11-04 22:29:38 +0600

    task app

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project_course/add_new_task_model.dart';
+import 'package:flutter_project_course/update_task_modal.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,25 +17,64 @@ class _HomePageState extends State<HomePage> {
         title: Text('ToDo'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return AddNewTaskModel();
+              });
         },
         child: Icon(Icons.add),
       ),
       body: ListView.separated(
         itemCount: 10,
-          itemBuilder: (context, index){
-        return ListTile(
-          leading: CircleAvatar(
-            child: Text('${index}'),
-          ),
-          title: Text('sdfer'),
-          subtitle: Text('12.10.2023'),
-          trailing: Text('pending'),
-
-        );
-
-    }, separatorBuilder: (BuildContext context, int index) {
-          return const Divider(); },),
+        itemBuilder: (context, index) {
+          return ListTile(
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text('Actions'),
+                      content: Column(
+                        children: [
+                          ListTile(
+                            leading: Icon(Icons.edit),
+                            title: Text('Update'),
+                            onTap: () {
+                              Navigator.pop(context);
+                              showModalBottomSheet(
+                                  context: context,
+                                  builder: (context) {
+                                    return UpdateTaskModal();
+                                  });
+                            },
+                          ),
+                          Divider(
+                            height: 0,
+                          ),
+                          ListTile(
+                            leading: Icon(Icons.delete_outline),
+                            title: Text('Delete'),
+                            onTap: () {},
+                          )
+                        ],
+                      ),
+                    );
+                  });
+            },
+            leading: CircleAvatar(
+              child: Text('${index}'),
+            ),
+            title: Text('sdfer'),
+            subtitle: Text('12.10.2023'),
+            trailing: Text('pending'),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return const Divider();
+        },
+      ),
     );
   }
 }
